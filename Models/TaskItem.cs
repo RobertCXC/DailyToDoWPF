fd94a19c9a584ddbda0beae7adcb58a1a85fc84c
MMG <53985815+RobertCXC@users.noreply.github.com>
2025-12-29 18:47:51 +0800

    勾选功能ok

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,9 +1,35 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace DailyToDo.Models
 {
-    public class TaskItem
+    public class TaskItem : INotifyPropertyChanged
     {
-        public string Title { get; set; } = string.Empty;
-        public bool IsCompleted { get; set; }
-        public bool IsImportant { get; set; }
+        private string _title = string.Empty;
+        public string Title
+        {
+            get => _title;
+            set { _title = value; OnPropertyChanged(); }
+        }
+
+        private bool _isCompleted;
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set { _isCompleted = value; OnPropertyChanged(); }
+        }
+
+        private bool _isImportant;
+        public bool IsImportant
+        {
+            get => _isImportant;
+            set { _isImportant = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? key = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(key));
+        }
     }
 }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -24,6 +24,68 @@ namespace DailyToDo.ViewModels
                 new TaskItem { Title = "多想学怪冷新增肯", IsCompleted = false },
                 new TaskItem { Title = "程序陈奕迅抽或许", IsCompleted = false }
             };
+
+            foreach (var task in Tasks)
+            {
+                task.PropertyChanged += Task_PropertyChanged;
+            }
+
+            Tasks.CollectionChanged += Tasks_CollectionChanged;
+        }
+
+        private void Tasks_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (TaskItem item in e.NewItems)
+                {
+                    item.PropertyChanged += Task_PropertyChanged;
+                }
+            }
+            if (e.OldItems != null)
+            {
+                foreach (TaskItem item in e.OldItems)
+                {
+                    item.PropertyChanged -= Task_PropertyChanged;
+                }
+            }
+        }
+
+        private void Task_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(TaskItem.IsCompleted))
+            {
+                if (sender is TaskItem task)
+                {
+                    // Delay execution to let the binding update complete, though not strictly necessary for simple property changes
+                    // Using Dispatcher or just running directly. 
+                    // Direct manipulation of ObservableCollection is usually fine on UI thread.
+                    MoveTask(task);
+                }
+            }
+        }
+
+        private void MoveTask(TaskItem task)
+        {
+            var oldIndex = Tasks.IndexOf(task);
+            if (oldIndex < 0) return;
+
+            if (task.IsCompleted)
+            {
+                // Move to bottom
+                if (oldIndex < Tasks.Count - 1)
+                {
+                    Tasks.Move(oldIndex, Tasks.Count - 1);
+                }
+            }
+            else
+            {
+                // Move to top
+                if (oldIndex > 0)
+                {
+                    Tasks.Move(oldIndex, 0);
+                }
+            }
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
